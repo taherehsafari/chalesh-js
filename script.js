30285696a26b29ce0049003fad30d763b62fbeef
tahereh <tahereh@gamil.com>
2023-12-11 07:55:50 -0800

    chalesh arry and objects

---
+++ script.js
@@ -0,0 +1,75 @@
+let school = [
+    
+    {
+
+        info : {
+            totalStudent : 4,
+            teacherName : "Khateri",
+            className : "classA"
+        },
+
+        students : [
+
+            {
+                name: "tahereh",
+                score: 17,
+                codeMeli: 5555555,
+            },
+
+            {
+                name: "fatemeh",
+                score: 18,
+                codeMeli: 6666666,
+            },
+
+            {
+                name: "paria",
+                score: 19,
+                codeMeli: 7777777,
+            },
+
+            student4 = {
+                name: "hossein",
+                score: 20,
+                codeMeli: 8888888,
+            },
+        ]
+    },
+
+    {
+
+        info : {
+            totalStudent : 4,
+            teacherName : "Ahmadi",
+            className : "classB"
+        },
+
+        students : [
+
+            {
+                name: "nazanin",
+                score: 20,
+                codeMeli: 9999999,
+            },
+
+            {
+                name: "rahele",
+                score: 19,
+                codeMeli: 4444444,
+            },
+
+            {
+                name: "dayan",
+                score: 17,
+                codeMeli: 3333333,
+            },
+
+            {
+                name: "maede",
+                score: 20,
+                codeMeli: 2222222,
+            },
+        ]
+    },
+]
+console.log(school[1])
